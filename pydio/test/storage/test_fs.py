d80c3ed2b24a64f82a0dcdcc78e90b56c4222b79
Louis Thibault <louis@pydio.com>
2017-05-31 11:36:08 +0200

    Added tests

--- pydio/test/storage/test_fs.py
+++ pydio/test/storage/test_fs.py
@@ -1,9 +1,39 @@
 #! /usr/bin/env python
 from twisted.trial.unittest import TestCase
 
-from zope.interface.verify import verifyClass
+from zope.interface.verify import verifyClass, DoesNotImplement
 
 from pydio.storage import fs, IStorage, IDiffHandler, ISelectiveEventHandler
+
+
+class TestLocalDirectory(TestCase):
+    def test_IStorage(self):
+        verifyClass(IStorage, fs.LocalDirectory)
+
+    def test_enforce_IStateManager(self):
+        self.assertRaises(
+            DoesNotImplement,
+            fs.LocalDirectory("/foo/bar/").connect_state_manager,
+            None,
+        )
+
+    def test_default_filter_param(self):
+        self.assertIsInstance(
+            fs.LocalDirectory("")._filt, dict,
+            "default parameter (None) does not initialize an empty dict",
+        )
+
+        self.assertNot(
+            fs.LocalDirectory("")._filt,
+            "default filter is not empty",
+        )
+
+    def test_custom_filter_param(self):
+        f = dict(include=["*"], exclude=[".*"])
+        self.assertEquals(
+            fs.LocalDirectory("", filters=f)._filt, f,
+            "LocalDirectory's filters do not match input dict",
+        )
 
 
 class TestEventHandler(TestCase):
@@ -12,8 +42,3 @@ class TestEventHandler(TestCase):
 
     def test_ISelectiveEventHandler(self):
         verifyClass(ISelectiveEventHandler, fs.EventHandler)
-
-
-class TestLocalDirectory(TestCase):
-    def test_IStorage(self):
-        verifyClass(IStorage, fs.LocalDirectory)
